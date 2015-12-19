184496a653695f0fded06ed900ed265b968bd1d6
tigredonorte <tigredonorte3@gmail.com>
2015-12-19 12:24:54 -0200

    geradorFixed

--- converter/classes/converterController.php
+++ converter/classes/converterController.php
@@ -4,6 +4,14 @@ class converterController extends \classes\Controller\Admin{
     
     public function index(){
         if(!empty ($_POST)){
+            $this->LoadResource('database', 'db');
+            $this->db->executeQuery("truncate gerador_gadget;");
+            $this->db->executeQuery("truncate gerador_componente;");
+            $this->db->executeQuery("truncate gerador_gadget;");
+            $this->db->executeQuery("truncate gerador_gdados;");
+            $this->db->executeQuery("truncate gerador_plugin;");
+            $this->db->executeQuery("truncate gerador_produto;");
+            $this->db->executeQuery("truncate gerador_widget;");
             //$this->registerVar('parsed', $this->model->InsertDesign($_POST));
             $this->model->InsertDesign($_POST);
             $this->setVars($this->model->getMessages());
